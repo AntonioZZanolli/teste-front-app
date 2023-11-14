05d8e7b551bfab40c34e102853c9085bd55f1fc6
Antônio Zilli Zanolli <antoniozzanolli@gmail.com>
2023-11-14 19:18:57 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import api, { IDataRequest, IDataResponse } from './provider/api';
 
 function App() {
+  const [clientes, setClientes] = useState<any>([]);
+
+  const carregarClientes = async () => {
+    const request: IDataRequest = {
+      url: "/clientes/"
+    }
+
+    const response: IDataResponse = await api.get(request);
+    if (response.statusCode === 200) {
+      setClientes(response.data)
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => { carregarClientes() }}>
+        Carregar
+      </button>
     </div>
   );
 }
